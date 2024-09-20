7c92fb23943c628c31831215accba9b3674f5054
theblaisecode <theblaisecode@gmail.com>
2024-09-20 10:35:16 +0100

    first commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,30 +4,59 @@ import { TbUserFilled } from "react-icons/tb";
 import { HiPaintBrush } from "react-icons/hi2";
 import ContactFormWrapper from "./ContactFormWrapper";
 import Button from "../Button";
-// import { Form } from "react-router-dom";
+import { Form } from "react-router-dom";
+import emailjs from "emailjs-com";
 
 function ContactForm({ sectionHead, sectionDescription }) {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_eqa2bwo",
+        "template_89texri",
+        e.target,
+        "ykYQxpSvmEm_6kHaM"
+      )
+      .then(
+        (result) => {
+          alert("Email successfully sent to the user!");
+        },
+        (error) => {
+          console.error(error.text);
+          alert("Failed to send email. Please try again.");
+        }
+      );
+
+    e.target.reset(); 
+  };
+
   return (
     <ContactFormWrapper>
       <div id="contactForm">
         <div className="contactFormContent">
           <HeadingTop head={sectionHead} description={sectionDescription} />
-
           <div className="contentBottom">
-            <form action="">
+            <Form onSubmit={sendEmail}>
               <div className="formInfo">
                 <div className="name">
                   <TbUserFilled />
-                  <input type="text" name="name" id="name" placeholder="Name" />
+                  <input
+                    type="text"
+                    name="from_name"
+                    id="name"
+                    placeholder="Name"
+                    required
+                  />
                 </div>
-
                 <div className="email">
                   <IoMail />
                   <input
-                    type="text"
+                    type="email"
                     name="email"
                     id="email"
                     placeholder="Email"
+                    required
                   />
                 </div>
               </div>
@@ -38,14 +67,15 @@ function ContactForm({ sectionHead, sectionDescription }) {
                   <textarea
                     name="message"
                     id="message"
-                    placeholder="Message"></textarea>
+                    placeholder="Message"
+                    required></textarea>
                 </div>
               </div>
 
               <div className="buttons">
                 <Button buttonText="Submit" />
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </div>
