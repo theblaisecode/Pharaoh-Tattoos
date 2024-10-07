3120af2b457831001e72a55d252fa7917f534613
theblaisecode <theblaisecode@gmail.com>
2024-10-07 19:23:21 +0100

    first commit

--- src/components/BackToTopButton.jsx
+++ src/components/BackToTopButton.jsx
@@ -32,6 +32,30 @@ const BtnStyling = styled.button`
     padding: 1rem 0.5rem;
     font-size: 1.5rem;
   }
+
+  @media (min-width: 551px) and (max-width: 950px) {
+    bottom: 12rem;
+    padding: 1.2rem 0.5rem;
+    font-size: 1.8rem;
+  }
+
+  @media (min-width: 951px) and (max-width: 1200px) {
+    bottom: 12rem;
+    right: 3rem;
+    padding: 1.2rem 0.5rem;
+    font-size: 1.5rem;
+  }
+
+  @media (min-width: 1201px) and (max-width: 1440px) {
+    bottom: 12rem;
+    right: 2.5rem;
+    padding: 1.2rem 0.5rem;
+    font-size: 1.8rem;
+  }
+
+  @media (min-width: 1441px) and (max-width: 1628px) {
+    bottom: 12rem;
+  }
 `;
 
 function BackToTopButton({ backToTop, scrollToTop }) {
