6d87168995f647a9f3530e8b43ced859a53610c6
theblaisecode <theblaisecode@gmail.com>
2024-11-04 07:10:35 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Pharaoh-Tattoos",
+  // base: "/Pharaoh-Tattoos",
   build: {
     outDir: "dist", // Explicitly specify the output directory
   },
