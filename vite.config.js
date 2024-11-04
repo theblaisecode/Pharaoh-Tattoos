1da5cf7de1cf41f835d5bf18babe5a26410062e0
theblaisecode <theblaisecode@gmail.com>
2024-11-04 09:12:09 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   base: "/Pharaoh-Tattoos",
-  
+  server: {
+    historyApiFallback: true,
+  },
 });
